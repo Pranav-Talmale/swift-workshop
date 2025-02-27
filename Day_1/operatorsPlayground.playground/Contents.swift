cdeefd1bb813aa48a1b0e7cf59ba33d569b15fdd
Pranav-Talmale <pranavtalmale@gmail.com>
2025-02-27 15:21:14 +0530

    Learn about operators in Swift

---
+++ Day_1/operatorsPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+let x = 51
+let y = 4
+let z = x/y
+print(z)
+
+let xD: Double = 51
+let yD: Double = 4
+let zD = xD/yD
+print(zD)
+
+var myScore = 10
+print(myScore)
+myScore += 3
+print(myScore)
+myScore -= 5
+print(myScore)
+myScore *= 10
+print(myScore)
+myScore /= 2
+print(myScore)
