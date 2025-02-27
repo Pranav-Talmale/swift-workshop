8b07d7e495b273cf63fb3b5ea730ac72076356b2
Pranav-Talmale <pranavtalmale@gmail.com>
2025-02-27 15:21:36 +0530

    Complete operators exercise

--- exercises/1 - Getting Started/3 - Operators/Lab - Operators.playground/Pages/7. Exercise - Numeric Type Conversion.xcplaygroundpage/Contents.swift
+++ exercises/1 - Getting Started/3 - Operators/Lab - Operators.playground/Pages/7. Exercise - Numeric Type Conversion.xcplaygroundpage/Contents.swift
@@ -3,13 +3,16 @@
 
  Create an integer constant `x` with a value of 10, and a double constant `y` with a value of 3.2. Create a constant `multipliedAsIntegers` equal to `x` times `y`. Does this compile? If not, fix it by converting your `Double` to an `Int` in the mathematical expression. Print the result.
  */
-
+let x = 10
+let y = 3.2
+// Binary operator '*' cannot be applied to operands of type 'Int' and 'Double'
+let multipliedAsIntegers = x*Int(y)
 
 //:  Create a constant `multipliedAsDoubles` equal to `x` times `y`, but this time convert the `Int` to a `Double` in the expression. Print the result.
-
+let multipliedAsDoubles = Double(x)*y
 
 //:  Are the values of `multipliedAsIntegers` and `multipliedAsDoubles` different? Print a statement to the console explaining why.
-
+print("The values of both the constants are different because in multipliedAsIntegers, value of y is being approximated to an integer while in multipliedAsDoubles, it does not get approximated leading to a more precise value.")
 
 /*:
 [Previous](@previous)  |  page 7 of 8  |  [Next: App Exercise - Converting Types](@next)
