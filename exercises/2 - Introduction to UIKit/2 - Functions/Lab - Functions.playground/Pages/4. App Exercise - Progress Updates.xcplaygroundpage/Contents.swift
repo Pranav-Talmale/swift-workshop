d803379fbb94d8dce2750f54d061ed2794194c80
Pranav-Talmale <pranavtalmale@gmail.com>
2025-03-01 12:51:04 +0530

    Day 3

--- exercises/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
+++ exercises/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
@@ -9,10 +9,39 @@
  
  Call the function a number of times, passing in different values of `steps` and `goal`. Observe the printouts and make sure what is printed to the console is what you would expect for the parameters passsed in.
  */
+func progressUpdate(steps: Int, goal: Int) -> Int{
+    if (((Double(steps)/Double(goal)) * 100) <= 10){
+        print("You're off to a good start.")
+        return 0
+    }
+    else if (((Double(steps)/Double(goal)) * 100) < 50){
+        print("You're almost halfway there!")
+        return 0
+    }
+    else if (((Double(steps)/Double(goal)) * 100) < 90){
+        print("You're over halfway there!")
+        return 0
+    }
+    else if (steps < goal){
+        print("You're almost there!")
+        return 0
+    }
+    else{
+        print("You beat your goal!")
+        return 0
+    }
+}
 
-
+progressUpdate(steps: 6000, goal: 10000)
 //:  Your fitness tracking app is going to help runners stay on pace to reach their goals. Write a function called pacing that takes four `Double` parameters called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. Your function should calculate whether or not the user is on pace to hit or beat `goalTime`. If yes, print "Keep it up!", otherwise print "You've got to push it just a bit harder!"
+func pacing(currentDistance: Double, totalDistance: Double, currentTime: Double, goalTime: Double) {
+    let currentSpeed = currentDistance/currentTime
+    let goalSpeed = totalDistance/goalTime
+    
+    currentSpeed >= goalSpeed ? print("Keep it up!") : print("You've got to push it just a bit harder!")
+}
 
+pacing(currentDistance: 600, totalDistance: 1000, currentTime: 50, goalTime: 100)
 
 /*:
 [Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Return Values](@next)
